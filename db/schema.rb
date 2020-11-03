47a56989fba5b2f07637c4f719744745e3c0c1c9
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-11-02 22:35:47 -0500

    created the event migration table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_230020) do
+ActiveRecord::Schema.define(version: 2020_11_03_032920) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "phone_number"
+    t.string "dress_code"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
